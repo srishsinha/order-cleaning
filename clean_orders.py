382b25f43f110dfb25f3b1333177ff2c713280ce
Sinha <srishsinha@deloitte.com>
2025-08-16 12:40:01 +0530

    Modified py script

--- clean_orders.py
+++ clean_orders.py
@@ -5,3 +5,6 @@ df = pd.read_csv('orders.csv')
 cleaned_df = df[df['amount']!=0]
 
 cleaned_df.to_csv('cleaned_orders.csv', index = False)
+
+total_amount = cleaned_df['amount'].sum()
+print(f"Total amount of cleaned orders: {total_amount}")
